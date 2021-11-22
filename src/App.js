e7af659a6015aa7f54ab0d48019f655eeec47dc5
joao <jlucasoc@hotmail.com>
2021-11-22 11:48:18 -0300

    Putting gliter ✨😘

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import "./App.css";
 
 function App() {
-    return <div className="App">Agata day</div>;
+    return (
+        <div className="App">
+            <section id="rotating-section">
+                <div id="rotating-animation">
+                    <div>
+                        <canvas id="birthday"></canvas>
+                        <div class="rect1"></div>
+                        <div class="rect4"></div>
+                        <div class="rect5"></div>
+                        <div class="rect6"></div>
+                        <div class="rect7"></div>
+                        <div class="rect9"></div>
+                        <div class="rect11"></div>
+                        <div class="rect12"></div>
+                        <div class="rect13"></div>
+                        <div class="rect14"></div>
+                        <div class="agata">Agata BDAY</div>
+                        <div class="agata2">Agata BDAY</div>
+                        <div class="agata3">Agata BDAY</div>
+                        <div class="rect16"></div>
+                        <div class="rect17"></div>
+                        <div class="rect19"></div>
+                        <div class="rect20"></div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
 }
 
 export default App;
